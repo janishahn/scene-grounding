ea959c910d50081cd79ed4cc57945b4dcfdc3322
1999Markus <markus.loehde@gmx.de>
2025-05-26 12:39:19 +0200

    rename

--- vlm_caption/model_handler.py
+++ vlm_caption/model_handler.py
@@ -6,7 +6,7 @@ from PIL import Image
 import torch
 import ollama
 
-class ModelHandler:
+class VLMHandler:
     """
     Unified interface for image captioning backends.
     Supported backends: 'transformers', 'ollama'.
